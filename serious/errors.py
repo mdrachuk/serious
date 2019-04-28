e1672f03dab43fb00e6a673d29f352b61bd435ae
mdrachuk <misha@drach.uk>
2019-04-28 19:56:31 +0300

    Detail the SerializationError

--- serious/errors.py
+++ serious/errors.py
@@ -1,13 +1,13 @@
-from typing import Type, List, Mapping, Collection
+from typing import Type, Mapping, Collection
 
 from serious.utils import DataClass, _class_path
 
 
 class SerializationError(Exception):
     def __init__(self, cls: Type[DataClass], serializer_stack: Collection[str]):
+        super().__init__()
         self._cls = cls
         self._path = self.__parse_stack(serializer_stack)
-        super().__init__(self._message())
 
     @staticmethod
     def __parse_stack(serializer_stack: Collection[str]) -> str:
@@ -15,26 +15,33 @@ class SerializationError(Exception):
             return ''
         return ''.join(serializer_stack)[1:]
 
-    def _message(self):
+    @property
+    def message(self):
         return f'Error during serialization of "{self._cls}"'
+
+    def __str__(self):
+        exc_type = super().__str__()
+        return f'{exc_type}: {self.message}'
 
 
 class LoadError(SerializationError):
     def __init__(self, cls: DataClass, serializer_stack: Collection[str], data: Mapping):
-        self._data = data
         super().__init__(cls, serializer_stack)
+        self._data = data
 
-    def _message(self):
-        return f'Failed to load "{self._path}" of {_class_path(self._cls)} from {self._data}'
+    @property
+    def message(self):
+        return f'Failed to load "{self._path}" of {_class_path(self._cls)} from {self._data}: {self.__cause__}'
 
 
 class DumpError(SerializationError):
     def __init__(self, obj: DataClass, serializer_stack: Collection[str]):
-        self._object = obj
         super().__init__(type(obj), serializer_stack)
+        self._object = obj
 
-    def _message(self):
-        return f'Failed to dump "{self._path}" of {self._object}'
+    @property
+    def message(self):
+        return f'Failed to dump "{self._path}" of {self._object}: {self.__cause__}'
 
 
 class UnexpectedItem(Exception):
