dd08a6b8c220af94e9729e285259d4957698dbd9
mdrachuk <misha@drach.uk>
2019-05-05 12:16:49 +0300

    Add package links and classifiers

--- setup.py
+++ setup.py
@@ -15,12 +15,26 @@ setup(
     url="https://github.com/mdrachuk/serious",
     license="Unlicense",
     keywords="dataclasses json serialization",
-    install_requires=[
-        "dataclasses;python_version=='3.7'",
-    ],
     python_requires=">=3.7",
+    project_urls={
+        'Pipelines': 'https://dev.azure.com/misha-drachuk/serious',
+        'Source': 'https://github.com/mdrachuk/serious/',
+    },
     extras_require={
         "dev": ["pytest", "mypy", "hypothesis"]
     },
-    include_package_data=True
+    include_package_data=True,
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Natural Language :: English",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: POSIX",
+        "Operating System :: POSIX :: BSD",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: Microsoft :: Windows",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: Implementation :: CPython",
+    ],
 )
