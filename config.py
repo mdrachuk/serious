39affb0ee278186010d6d8b1a38ae21ec16983b5
mdrachuk <misha@drach.uk>
2019-05-05 17:15:00 +0300

    Bump version

--- config.py
+++ config.py
@@ -17,7 +17,7 @@ class Config:
 
 config = Config(
     name='serious',
-    version='2019.5.dev4',
+    version='2019.5.dev5',
     readme_path='README.md',
     author='mdrachuk',
     author_email='misha@drach.uk'
