063e864bb9593412cee0e8c528a51d36b87f4204
mdrachuk <misha@drach.uk>
2019-07-15 14:11:26 +0300

    Add allow_any tests

--- tests/test_any.py
+++ tests/test_any.py
@@ -3,7 +3,11 @@ from datetime import datetime
 from decimal import Decimal
 from typing import Any, Dict, List
 
+import pytest
+
 from serious.dict import DictSchema
+from serious.errors import ModelContainsAny
+from serious.json import JsonSchema
 
 
 @dataclass(frozen=True)
@@ -17,7 +21,7 @@ class User:
 class TestAny:
 
     def setup_class(self):
-        self.schema = DictSchema(User)
+        self.schema = DictSchema(User, allow_any=True)
         now = datetime.now()
         height = Decimal('1.76')
 
@@ -43,3 +47,57 @@ class TestAny:
     def test_nested_implicit_any_dump(self):
         actual = self.schema.dump(self.albert_meta)
         assert actual['meta'] == self.meta
+
+
+@dataclass(frozen=True)
+class Something:
+    contents: Any
+
+
+@dataclass(frozen=True)
+class Closet:
+    items: list
+
+
+@dataclass(frozen=True)
+class Thing:
+    name: str
+
+
+class TestAllowAnyInJson:
+
+    def test_default(self):
+        assert DictSchema(Thing)
+        with pytest.raises(ModelContainsAny):
+            DictSchema(Something)
+
+    def test_explicit_with_any(self):
+        assert DictSchema(Something, allow_any=True)
+        with pytest.raises(ModelContainsAny):
+            assert DictSchema(Something, allow_any=False)
+
+    def test_explicit_without_any(self):
+        assert DictSchema(Thing, allow_any=True)
+        assert DictSchema(Thing, allow_any=False)
+
+    def test_explicit_with_collection_of_any(self):
+        assert DictSchema(Closet, allow_any=True)
+        with pytest.raises(ModelContainsAny):
+            assert DictSchema(Closet, allow_any=False)
+
+
+class TestAllowAnyInDict:
+
+    def test_default(self):
+        assert JsonSchema(Thing)
+        with pytest.raises(ModelContainsAny):
+            JsonSchema(Something)
+
+    def test_explicit_with_any(self):
+        assert JsonSchema(Something, allow_any=True)
+        with pytest.raises(ModelContainsAny):
+            assert JsonSchema(Something, allow_any=False)
+
+    def test_explicit_without_any(self):
+        assert JsonSchema(Thing, allow_any=True)
+        assert JsonSchema(Thing, allow_any=False)
