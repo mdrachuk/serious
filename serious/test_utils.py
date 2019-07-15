822ee6187562cf817c88c7ad27c92bbb95610e80
mdrachuk <misha@drach.uk>
2019-07-15 15:28:49 +0300

    Fix typings

--- serious/test_utils.py
+++ serious/test_utils.py
@@ -1,4 +1,4 @@
-from typing import Union, Any
+from typing import Any, overload
 
 from serious.dict import DictSchema
 from serious.json import JsonSchema
@@ -6,6 +6,16 @@ from serious.json import JsonSchema
 Dataclass = Any  # a dataclass instance
 
 
-def assert_symmetric(serializer: Union[JsonSchema, DictSchema], value: Dataclass):
+@overload
+def assert_symmetric(serializer: DictSchema, value: Dataclass):
+    pass
+
+
+@overload
+def assert_symmetric(serializer: JsonSchema, value: Dataclass):
+    pass
+
+
+def assert_symmetric(serializer, value):
     """Asserts that dumping an instance of dataclass via schema and loading it will result in an equal object."""
     assert serializer.load(serializer.dump(value)) == value, f'load/dump are not symmetric in {serializer}.'
