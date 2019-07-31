c3390b27f2e0bc318bdec1b9d873778f26cdfd19
mdrachuk <misha@drach.uk>
2019-07-31 11:38:51 +0300

    Document check_version

--- check_version.py
+++ check_version.py
@@ -1,13 +1,21 @@
+import requests
+
+__doc__ = """
+This module is querying PyPI to check if the current version set to package is already present on PyPI.
+
+Used during PR checks, to ensure that package version is changed. 
+
+Finishes with an VersionExists exception and a non-zero exit code if the version exists on PyPI. 
+"""
 from dataclasses import dataclass
 from typing import Dict, Set, List
-from urllib import request
 
-from config import config
+from config import Config
 from serious.json import JsonSchema
 
 
 @dataclass
-class Package:
+class PypiPackage:
     releases: Dict[str, List]
 
     @property
@@ -21,6 +29,11 @@ class Package:
 
 
 def simplify(value: str) -> str:
+    """
+    PyPI simplifies the package version to be uniformly formatted.
+    It does that without any notice.
+    This emulates such an operation.
+    """
     value = value.replace('.', '')
     value = value.replace('-', '')
     value = value.replace('_', '')
@@ -34,14 +47,20 @@ class VersionExists(Exception):
                          f'You can change the version in "config.py".')
 
 
-def check_exists(pkg_config):
-    package_schema = JsonSchema(Package, allow_unexpected=True, allow_any=True)
-    with request.urlopen('https://pypi.org/pypi/serious/json') as pypi:
-        package_data = pypi.read()
-    package = package_schema.load(package_data)
-    if package.contains_version(pkg_config.version):
-        raise VersionExists(pkg_config)
+def fetch_package(package_name: str):
+    schema = JsonSchema(PypiPackage, allow_unexpected=True, allow_any=True)
+    package_json = requests.get(f'https://pypi.org/pypi/{package_name}/json').text
+    return schema.load(package_json)
+
+
+def check_unique(pkg: Config):
+    remote = fetch_package(pkg.name)
+    if remote.contains_version(pkg.version):
+        raise VersionExists(pkg)
+    print(f'OK: {pkg.name} {pkg.version} is not present on PyPI.')
 
 
 if __name__ == '__main__':
-    check_exists(config)
+    from config import config
+
+    check_unique(config)
