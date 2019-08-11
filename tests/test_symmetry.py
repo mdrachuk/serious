24be718655823c7a50b92297d87e0c51f25424e1
mdrachuk <misha@drach.uk>
2019-08-11 10:15:09 +0300

    Get rid of hypothesis

--- tests/test_symmetry.py
+++ tests/test_symmetry.py
@@ -1,23 +1,22 @@
-from hypothesis import given
-from hypothesis.strategies import frozensets, integers, lists, one_of, sets, tuples
+from typing import List, Set, Tuple, Optional, FrozenSet
 
-from serious import JsonModel
+from serious import JsonModel, DictModel
 from serious.test_utils import assert_symmetric
-from tests.entities import (DataclassWithFrozenSet, DataclassWithList, DataclassWithOptional,
-                            DataclassWithSet, DataclassWithTuple)
-from tests.hypothesis2 import examples
-from tests.hypothesis2.strategies import optionals
+from tests.entities import dataclass_of
+from tests.utils import with_
 
-dcconss_strategies_conss = [
-    (DataclassWithList, lists, list, [1]),
-    (DataclassWithSet, sets, set, [1]),
-    (DataclassWithTuple, tuples, tuple, [1]),
-    (DataclassWithFrozenSet, frozensets, frozenset, [1]),
-    (DataclassWithOptional, optionals, lambda x: x, 1)
+models = [JsonModel, DictModel]
+objects = [
+    dataclass_of(List[int])([1, 2, 3]),
+    dataclass_of(Set[int])({1, 2, 3}),
+    dataclass_of(Tuple[int, Ellipsis])(tuple([1, 2, 3])),
+    dataclass_of(Tuple[int, str, int])(tuple([1, 'two', 3])),
+    dataclass_of(FrozenSet[int])(frozenset([1, 2, 3])),
+    dataclass_of(Optional[int])(None),
+    dataclass_of(Optional[int])(3),
 ]
 
 
-@given(one_of(*[strategy_fn(integers()).map(dccons) for dccons, strategy_fn, *_ in dcconss_strategies_conss]))
-@examples(*[dccons(cons(input)) for dccons, _, cons, input in dcconss_strategies_conss])
-def test_generic_encode_and_decode_are_inverses(dc):
-    assert_symmetric(JsonModel(type(dc)), dc)
+@with_(models, objects)
+def test_generic_encode_and_decode_are_inverses(new_model, dc):
+    assert_symmetric(new_model(type(dc)), dc)
---
+++ tests/utils.py
@@ -0,0 +1,26 @@
+import inspect
+from itertools import product
+from typing import Tuple, Sequence
+
+import pytest
+
+
+def with_(*collections: Tuple[Sequence], cartesian_product=True):
+    def _wrapped(f):
+        signature = inspect.signature(f)
+        func_params = list(signature.parameters)
+        if len(func_params) and func_params[0] == 'self':
+            func_params.pop(0)
+        pytest_keys = ','.join(func_params)
+        if len(collections) > 1:
+            if cartesian_product:
+                pytest_values = list(product(*collections))
+            else:
+                if len(set(map(len, collections))) != 1:
+                    raise Exception('Parameters must be of equal length')
+                pytest_values = list(zip(*collections))
+        else:
+            pytest_values = collections[0]
+        return pytest.mark.parametrize(pytest_keys, pytest_values)(f)
+
+    return _wrapped
