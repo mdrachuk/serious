8a7b177ba88930e9dfa4a50c17087da3d98aa251
mdrachuk <misha@drach.uk>
2019-08-15 21:41:52 +0300

    Document context

--- serious/serialization/context.py
+++ serious/serialization/context.py
@@ -1,3 +1,4 @@
+"""Loading/dumping context holding the current execution stack, calling serializers and validators."""
 from __future__ import annotations
 
 __all__ = ['Context', 'Loading', 'Dumping']
@@ -15,6 +16,14 @@ S = TypeVar('S')  # Serialized value
 
 
 class Context(ABC):
+    """An abstract base class for the serialization context.
+
+    Serialization context is created when executing `SeriousModel` load or dump and is passed inside
+    all nested serializers.
+
+    All of serializers are called via context to include them in stack and to perform
+    all the necessary validation and processing
+    """
 
     def __init__(self):
         self._steps: List[SerializationStep] = list()
@@ -27,14 +36,27 @@ class Context(ABC):
 
     @property
     def stack(self) -> FrozenList[SerializationStep]:
+        """The stack is included in errors, mentioning the fields, array indexes, dictionary keys, etc."""
         return FrozenList(self._steps)
 
     @abstractmethod
     def run(self, step: str, serializer: Serializer, value: Any) -> Any:
+        """Execute serializer in context.
+
+        Implementations:
+        - includes the current step in the stack,
+        - executes current steps serializer,
+        - performs any required processing of values.
+
+        This abstraction is needed for a straightforward custom serializer implementation.
+        Extracting validation (or any other value processing) from serializers to `Context#run(...)`
+        has left field serializers with a plain structure of a constructor, load, dump, and fits methods.
+        """
         raise NotImplementedError
 
 
 class Loading(Context):
+    """Context used during **load** operations."""
 
     def __init__(self, *, validating: bool):
         super().__init__()
@@ -49,7 +71,7 @@ class Loading(Context):
 
 
 class Dumping(Context):
-
+    """Context used during **dump** operations."""
     def __init__(self, *, validating: bool):
         super().__init__()
         self.validating = validating
